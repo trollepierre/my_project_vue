c410fc7d50ff6018d9a300e02bd04a6cbca65b7a
Pierre Trollé <ptrolle@octo.com>
2017-05-05 17:40:26 +0200

    arrange first test about data

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -2,10 +2,27 @@ import Vue from 'vue'
 import Hello from '@/components/Hello'
 
 describe('Hello.vue', () => {
-  it('should render correct contents', () => {
+  let vm
+
+  beforeEach(function () {
     const Constructor = Vue.extend(Hello)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
+    vm = new Constructor().$mount()
   })
+
+  it('should check that msg is Welcome to Your Vue.js App', () => {
+    expect(vm.$data.msg).to.equal('Welcome to Your Vue.js App')
+  })
+
+  it('should render correct contents', () => {
+    const data = {
+      data: {
+        msg: 'plop'
+      }
+    }
+    const Constructor = Vue.extend(Hello)
+    vm = new Constructor(data).$mount()
+    expect(vm.$el.querySelector('.hello h1').textContent)
+      .to.equal('plop')
+  })
+
 })
