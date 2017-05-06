edd9f0e5d3b42ce2a6e9b1e4ebb6c8f9c98b82de
Pierre Trollé <ptrolle@octo.com>
2017-05-06 09:22:06 +0200

    add button inside ClickMeButton.vue

--- test/unit/specs/ClickMeButton.spec.js
+++ test/unit/specs/ClickMeButton.spec.js
@@ -12,4 +12,9 @@ describe('ClickMeButton.vue', () => {
   it('should check the name of my vue', () => {
     expect(vm.$options.name).to.equal('clickMeButton')
   })
+
+  it('should render button with text Click Me Button', () => {
+    expect(vm.$el.querySelector('.clickMeButton button').textContent)
+      .to.equal('Click Me Button')
+  })
 })
